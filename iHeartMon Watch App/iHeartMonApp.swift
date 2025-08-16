b26dddb6e034ba19f41731051b306305eb8c8163
Laura Money <LauraC.Money@gmail.com>
2025-08-16 11:40:18 -0700

    Initial Commit

---
+++ iHeartMon Watch App/iHeartMonApp.swift
@@ -0,0 +1,17 @@
+//
+//  iHeartMonApp.swift
+//  iHeartMon Watch App
+//
+//  Created by Laura Money on 8/16/25.
+//
+
+import SwiftUI
+
+@main
+struct iHeartMon_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
